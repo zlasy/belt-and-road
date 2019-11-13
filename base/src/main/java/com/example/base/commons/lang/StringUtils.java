903e0e67c564bef086239445b9bed12471ddff37
zhangle <zhangle@dangdang.com>
2019-11-13 17:00:07 +0800

    string similarity

--- base/src/main/java/com/example/base/commons/lang/StringUtils.java
+++ base/src/main/java/com/example/base/commons/lang/StringUtils.java
@@ -1,17 +1,17 @@
 package com.example.base.commons.lang;
 
-        import org.apache.commons.text.similarity.JaccardSimilarity;
-        import org.apache.commons.text.similarity.JaroWinklerDistance;
+import org.apache.commons.text.similarity.JaccardSimilarity;
+import org.apache.commons.text.similarity.JaroWinklerDistance;
 
 public class StringUtils {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         JaroWinklerDistance distance = new JaroWinklerDistance();
         System.out.println(distance.apply("中华人民共和国", "中华民国"));
 
         JaccardSimilarity similarity = new JaccardSimilarity();
-        System.out.println(similarity.apply("中华人民共和国","中华民国"));
+        System.out.println(similarity.apply("中华人民共和国", "中华民国"));
 
     }
 }
